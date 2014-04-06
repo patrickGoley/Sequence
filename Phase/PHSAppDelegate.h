6b4966535ff784a66fbe5233675da0e4a8c80fe1
Patrick Goley <patrickg@aloompa.com>
2014-04-06 12:43:37 -0500

    Initial Commit

---
+++ Phase/PHSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PHSAppDelegate.h
+//  Phase
+//
+//  Created by Pat Goley on 4/6/14.
+//  Copyright (c) 2014 Patrick Goley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PHSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Phase/PHSViewController.h
@@ -0,0 +1,13 @@
+//
+//  PHSViewController.h
+//  Phase
+//
+//  Created by Pat Goley on 4/6/14.
+//  Copyright (c) 2014 Patrick Goley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PHSViewController : UIViewController
+
+@end
